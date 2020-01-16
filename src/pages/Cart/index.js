24207895cc1a9abd47de145f77a30ca3dccc997b
Luis Camargo <luis.e.c.mendoza@gmail.com>
2020-01-16 16:53:34 -0300

    Bug

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -5,7 +5,33 @@ import {Container} from './styles.js';
 export default function Cart(){
     return (
         <Container>
-            
-        <Container/>    
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                          <img /> 
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type='button'>Finalizar pedido</button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1920,90</strong>
+                </Total>
+            </footer>
+        </Container> 
     )
 }
